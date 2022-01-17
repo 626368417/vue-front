75966f0556a5a5877eba9356284b205a69935a1e
mengsun <sunmeng@yunify.com>
2022-01-17 14:38:41 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for='item in list' :key='item.id'>
+        {{item.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  mounted(){
+   axios.get('http://localhost:3000/api/users').then(res=>{
+      console.log(res.data)
+       this.list = res.data
+   })
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-form
+    :model="dynamicValidateForm"
+    ref="dynamicValidateForm"
+    label-width="100px"
+    class="demo-dynamic"
+  >
+    <el-form-item
+      prop="email"
+      label="邮箱"
+      :rules="[
+        { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      ]"
+    >
+      <el-input v-model="dynamicValidateForm.email"></el-input>
+    </el-form-item>
+    <div v-for="(domain, index) in dynamicValidateForm.domains" :key="index">
+      <el-form-item
+        :label="'域名' + index"
+        :prop="'domains.' + index + '.value'"
+        :rules="{
+          required: true,
+          message: '域名不能为空',
+          trigger: 'blur',
+        }"
+      >
+        <el-input v-model="domain.value"></el-input>
+      </el-form-item>
+
+      <el-form-item
+        label="活动区域"
+        :prop="'domains.' + index + '.region'"
+        :rules="{
+          required: true,
+          message: '活动区域',
+          trigger: 'change',
+        }"
+      >
+        <el-select placeholder="请选择活动区域" v-model="domain.region">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="活动区域2"
+        :prop="'domains.' + index + '.region1'"
+        :rules="{
+          required: true,
+          message: '活动区域2',
+          trigger: 'change',
+        }"
+      >
+        <el-select placeholder="请选择活动区域" v-model="domain.region1">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </div>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('dynamicValidateForm')"
+        >提交</el-button
+      >
+      <el-button @click="addDomain">新增</el-button>
+      <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      dynamicValidateForm: {
+        domains: [
+          {
+            value: "",
+            region: "",
+            region1: "",
+          },
+        ],
+        email: "",
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    removeDomain(item) {
+      var index = this.dynamicValidateForm.domains.indexOf(item);
+      if (index !== -1) {
+        this.dynamicValidateForm.domains.splice(index, 1);
+      }
+    },
+    addDomain() {
+      this.dynamicValidateForm.domains.push({
+        value: "",
+        region: "",
+        region1: "",
+      });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
